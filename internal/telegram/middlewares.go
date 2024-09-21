3714f809aa5984915698d9425a288741026a3c08
Plague Fox <plugfox@gmail.com>
2024-09-21 16:26:26 +0400

    Refactor: Update user verification logic, add local ban check, and refactor middleware logic

--- internal/telegram/middlewares.go
+++ internal/telegram/middlewares.go
@@ -49,6 +49,130 @@ func kickUser(bot *tele.Bot, chat *tele.Chat, user *tele.User) error {
 	}, true)
 }
 
+// Verify the user with a local database and a CAS
+func isUserBanned(db *storage.Storage, bot *tele.Bot, user *tele.User) (bool, error) {
+	// Check local ban
+	banned, err := db.IsBannedUser(model.UserID(user.ID))
+	if err != nil {
+		return false, err
+	} else if banned {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+// Verify user middleware - verify the user with a captcha
+func verifyUserMiddleware(db *storage.Storage, config *config.Config, onError func(error)) tele.MiddlewareFunc {
+	return func(next tele.HandlerFunc) tele.HandlerFunc {
+		return func(c tele.Context) error {
+			if c.Callback() != nil {
+				return next(c) // Thats a callback, proceed to the next middleware / handler
+			}
+
+			sender := c.Sender()
+			chat := c.Chat()
+
+			// Check if the user is admin or the chat is private
+			if chat != nil {
+				member, err := c.Bot().ChatMemberOf(chat, sender)
+				if err == nil {
+					isAdmin := member.Role == tele.Creator || member.Role == tele.Administrator
+					if isAdmin || chat.Private {
+						return next(c) // Proceed to the next middleware if the user is admin or the chat is private
+					}
+				}
+			}
+
+			if sender.ID == 0 || chat.ID == 0 || sender.ID == chat.ID || sender.IsBot || chat.Private {
+				return nil // Ignore if the user ID or chat ID is not available or thats a PM
+			}
+
+			// Should we verify the user in this chat?
+			if !allowedChats(config, chat.ID) {
+				return nil // Ignore if the chat is not in the allowed chats list
+			}
+
+			verified, err := db.IsVerifiedUser(model.UserID(sender.ID))
+			if err != nil /* && onError != nil */ {
+				if onError != nil {
+					onError(err) // Log the error
+				}
+				return nil // Skip the current message
+			} else if verified {
+				return next(c) // Proceed to the next middleware if the user is verified
+			}
+
+			// Verify the user asynchronously
+			defer c.Delete() // Delete the message, because the user is not verified
+
+			bot := c.Bot()
+
+			banned, err := isUserBanned(db, bot, sender)
+			if err != nil {
+				if onError != nil {
+					onError(err) // Log the error
+				}
+				return nil
+			} else if banned {
+				// Ban the user again if they are already banned
+				err = bot.Ban(chat, &tele.ChatMember{User: sender}, true)
+				if err != nil {
+					if onError != nil {
+						onError(err) // Log the error
+					}
+					return nil
+				}
+				return nil // Skip the current message
+			}
+
+			// Check CAS ban
+			/* channel := make(chan error)
+			go verifyUserBan(channel, db, config, bot, chat, sender)
+			select {
+			case err := <-channel:
+				if err != nil && onError != nil {
+					onError(err) // Log the error
+				}
+			} */
+
+			return nil // Skip the current message
+		}
+	}
+}
+
+// storeMessages middleware - store messages in the database asynchronously
+func storeMessagesMiddleware(db *storage.Storage, onError func(error)) tele.MiddlewareFunc {
+	return func(next tele.HandlerFunc) tele.HandlerFunc {
+		return func(c tele.Context) error {
+			msg := c.Message()
+			if msg != nil {
+				go func() {
+					err := db.UpsertMessage(
+						storage.UpsertMessageInput{
+							Message: converters.MessageFromTG(msg),
+							Chats: []*model.Chat{
+								converters.ChatFromTG(msg.Chat),
+								converters.ChatFromTG(msg.SenderChat),
+								converters.ChatFromTG(msg.OriginalChat),
+							}, Users: []*model.User{
+								converters.UserFromTG(msg.Sender).Seen(),
+								converters.UserFromTG(msg.OriginalSender),
+								converters.UserFromTG(msg.Via),
+								converters.UserFromTG(msg.UserJoined),
+								converters.UserFromTG(msg.UserLeft),
+							},
+						})
+					if err != nil && onError != nil {
+						onError(err)
+					}
+				}()
+			}
+			return next(c)
+		}
+	}
+}
+
 // Verify the user with a captcha
 /* func verifyUserWithCaptcha(channel chan error, db *storage.Storage, config *config.Config, bot *tele.Bot, chat *tele.Chat, user *tele.User) {
 	banned, err := db.IsBannedUser(model.UserID(user.ID))
@@ -173,127 +297,3 @@ func kickUser(bot *tele.Bot, chat *tele.Chat, user *tele.User) error {
 		return nil
 	})
 } */
-
-// Verify the user with a local database and a CAS
-func isUserBanned(db *storage.Storage, bot *tele.Bot, user *tele.User) (bool, error) {
-	// Check local ban
-	banned, err := db.IsBannedUser(model.UserID(user.ID))
-	if err != nil {
-		return false, err
-	} else if banned {
-		return true, nil
-	}
-
-	return false, nil
-}
-
-// Verify user middleware - verify the user with a captcha
-func verifyUserMiddleware(db *storage.Storage, config *config.Config, onError func(error)) tele.MiddlewareFunc {
-	return func(next tele.HandlerFunc) tele.HandlerFunc {
-		return func(c tele.Context) error {
-			if c.Callback() != nil {
-				return next(c) // Thats a callback, proceed to the next middleware / handler
-			}
-
-			sender := c.Sender()
-			chat := c.Chat()
-
-			// Check if the user is admin or the chat is private
-			if chat != nil {
-				member, err := c.Bot().ChatMemberOf(chat, sender)
-				if err == nil {
-					isAdmin := member.Role == tele.Creator || member.Role == tele.Administrator
-					if isAdmin || chat.Private {
-						return next(c) // Proceed to the next middleware if the user is admin or the chat is private
-					}
-				}
-			}
-
-			if sender.ID == 0 || chat.ID == 0 || sender.ID == chat.ID || sender.IsBot || chat.Private {
-				return nil // Ignore if the user ID or chat ID is not available or thats a PM
-			}
-
-			// Should we verify the user in this chat?
-			if !allowedChats(config, chat.ID) {
-				return nil // Ignore if the chat is not in the allowed chats list
-			}
-
-			verified, err := db.IsVerifiedUser(model.UserID(sender.ID))
-			if err != nil /* && onError != nil */ {
-				if onError != nil {
-					onError(err) // Log the error
-				}
-				return nil // Skip the current message
-			} else if verified {
-				return next(c) // Proceed to the next middleware if the user is verified
-			}
-
-			// Verify the user asynchronously
-			defer c.Delete() // Delete the message, because the user is not verified
-
-			bot := c.Bot()
-
-			banned, err := isUserBanned(db, bot, sender)
-			if err != nil {
-				if onError != nil {
-					onError(err) // Log the error
-				}
-				return nil
-			} else if banned {
-				// Ban the user again if they are already banned
-				err = bot.Ban(chat, &tele.ChatMember{User: sender}, true)
-				if err != nil {
-					if onError != nil {
-						onError(err) // Log the error
-					}
-					return nil
-				}
-				return nil // Skip the current message
-			}
-
-			// Check CAS ban
-			/* channel := make(chan error)
-			go verifyUserBan(channel, db, config, bot, chat, sender)
-			select {
-			case err := <-channel:
-				if err != nil && onError != nil {
-					onError(err) // Log the error
-				}
-			} */
-
-			return nil // Skip the current message
-		}
-	}
-}
-
-// storeMessages middleware - store messages in the database asynchronously
-func storeMessagesMiddleware(db *storage.Storage, onError func(error)) tele.MiddlewareFunc {
-	return func(next tele.HandlerFunc) tele.HandlerFunc {
-		return func(c tele.Context) error {
-			msg := c.Message()
-			if msg != nil {
-				go func() {
-					err := db.UpsertMessage(
-						storage.UpsertMessageInput{
-							Message: converters.MessageFromTG(msg),
-							Chats: []*model.Chat{
-								converters.ChatFromTG(msg.Chat),
-								converters.ChatFromTG(msg.SenderChat),
-								converters.ChatFromTG(msg.OriginalChat),
-							}, Users: []*model.User{
-								converters.UserFromTG(msg.Sender).Seen(),
-								converters.UserFromTG(msg.OriginalSender),
-								converters.UserFromTG(msg.Via),
-								converters.UserFromTG(msg.UserJoined),
-								converters.UserFromTG(msg.UserLeft),
-							},
-						})
-					if err != nil && onError != nil {
-						onError(err)
-					}
-				}()
-			}
-			return next(c)
-		}
-	}
-}
